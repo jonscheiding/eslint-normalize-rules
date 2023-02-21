b46d4dec8ac50dbd5b6d04beabb607f3e30ed762
Jon Scheiding <jonscheiding@gmail.com>
2023-02-21 12:28:56 -0500

    documentation

---
+++ README.md
@@ -0,0 +1,27 @@
+# eslint-normalize-rules
+
+This package allows you to generate eslint configs that normalize all the configured rules to the same level (`warn` or `error`).
+
+This is for scenarios where you want to treat all warnings as errors (or vice versa) but you also want to extend configurations that have rules configured at both levels.
+
+## Usage
+
+Example `.eslintrc.js`:
+
+```javascript
+const { normalizeRules } = require("eslint-normalize-rules");
+
+const config = {
+  extends: [
+    "eslint:recommended",
+    "eslint-standard-with-typescript"
+  ],
+  rules: {
+    "no-var": "warn"
+  }
+}
+
+module.exports = normalizeRules(config);
+```
+
+This will set all rules to `error`, except for `no-var` because it is explicitly set at the config level.
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 import { resolve } from "path";
 import { Legacy } from "@eslint/eslintrc";
 
+/**
+ * Options type for rule normalization.
+ */
 interface Options {
+  /**
+   * The level to normalize rules to. Defaults to 'error'.
+   */
   level?: "warn" | "error";
+  /**
+   * If true, normalizes rules specified in the top-level config's `rules`
+   * property; otherwise, only normalizes rules inherited from extended configs.
+   * Defaults to false.
+   */
   includeExplicit?: boolean;
 }
 
+/**
+ * Helper interface for eslint config.
+ */
 interface Config {
+  /**
+   * The rules defined at the top level of the config.
+   */
   rules?: Record<string, any>;
 }
 
+/**
+ * Normalize the rules in an eslint configuration.
+ * Sets all rules to the same level ('error' by default) unless their level
+ * is explicitly specified in the config's own rules property.
+ *
+ * @param config The configuration
+ * @param options Options for rule normalization
+ * @returns
+ */
 export function normalizeRules<T extends Config>(
   config: T,
   options?: Options
